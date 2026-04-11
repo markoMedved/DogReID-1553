9c73f564dd8bd1a46f7cf8769b71758e8ebb464c
Marko Medved <mm12755@hpc-login3.arnes.si>
2026-04-11 09:27:01 +0200

    add job.sh2

---
+++ job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=doggy_mega_infer
+#SBATCH --output=logs/infer_%j.out
+#SBATCH --error=logs/infer_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=96G
+
+# Load your conda function
+source $(conda info --base)/etc/profile.d/conda.sh
+
+# Go to your code directory
+cd /d/hpc/projects/FRI/mm12755/DogReID-1553/DogReID-1553
+
+# Activate the env
+conda activate project
+
+echo "Running on $(hostname) with $(nvidia-smi --query-gpu=name --format=csv,noheader)"
+
+# Finally launch
+python train.py 
